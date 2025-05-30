a1998a8816879549ad381c6942eba9bba4abf4d0
Sara Patti <sep22@ic.ac.uk>
2025-05-30 11:44:05 +0100

    updated paths, logging information, and mistake in adata.write function

--- src/1_qc.py
+++ src/1_qc.py
@@ -29,6 +29,7 @@ import logging
 
 # Set variables
 # ? How should I config this so a user can easily change them?
+module_name = "1_qc"  # name of the module
 min_counts = 10
 min_cells = 5
 
@@ -39,9 +40,23 @@ output_path = "/Users/sarapatti/Desktop/PhD_projects/Llyod_lab/ReCoDe-spatial-tr
 zarr_path = Path(input_path) / "data/xenium.zarr"
 logging_path = "/Users/sarapatti/Desktop/PhD_projects/Llyod_lab/ReCoDe-spatial-transcriptomics/analysis/logging"
 
+# Confirm directories exist
+if not Path(input_path).exists():
+    raise FileNotFoundError(f"Input path {input_path} does not exist.")
+if not Path(output_path).exists():
+    raise FileNotFoundError(f"Output path {output_path} does not exist.")
+if not Path(zarr_path).exists():
+    raise FileNotFoundError(f"Zarr path {zarr_path} does not exist.")
+
+# Create output directories if they do not exist
+os.makedirs(Path(output_path) / module_name, exist_ok=True)
+
 # Set up logging
+os.makedirs(
+    logging_path, exist_ok=True
+)  # should set up all these directories at the start of the pipeline?
 logging.basicConfig(
-    filename=Path(logging_path) / "1_qc.txt",  # output file
+    filename=Path(logging_path) / f"{module_name}.txt",  # output file
     filemode="w",  # overwrites the file each time
     format="%(asctime)s - %(levelname)s - %(message)s",  # log format
     level=logging.INFO,  # minimum level to log
@@ -124,8 +139,11 @@ sns.histplot(
 
 # Adjust layout and save the figure
 plt.tight_layout()
-plt.savefig(Path(output_path) / "1_qc/cell_summary_histograms.png", dpi=300)
+plt.savefig(
+    Path(output_path) / f"{module_name}/cell_summary_histograms.png", dpi=300
+)
 plt.close()
+logging.info(f"Saved plots to {module_name}/cell_summary_histograms.png")
 
 # $ QC data #
 
@@ -136,12 +154,12 @@ sc.pp.filter_genes(adata, min_cells=min_cells)
 
 
 # Normalize data
+logging.info("Normalize data...")
 adata.layers["counts"] = adata.X.copy()  # make copy of raw data
 sc.pp.normalize_total(adata, inplace=True)  # normalize data
-# Log transform data
-sc.pp.log1p(adata)
+sc.pp.log1p(adata)  # Log transform data
 
 
 # Save data
-adata.write_h5ad(adata, Path(output_path) / "1_qc/adata.h5ad")
-logging.info("Data saved to 1_qc/adata.h5ad")
+adata.write_h5ad(Path(output_path) / f"{module_name}/adata.h5ad")
+logging.info(f"Data saved to {module_name}/adata.h5ad")

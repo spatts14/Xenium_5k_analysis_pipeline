afced4606bdac27bacc4e0c5ea90104c13ce74b3
Sara Patti <sep22@ic.ac.uk>
2025-06-04 17:32:12 +0100

    updated to make directories if they dont exist

--- src/recode_st/paths.py
+++ src/recode_st/paths.py
@@ -9,3 +9,13 @@ output_path = base_dir / "analysis"
 xenium_path = data_path / "xenium"
 zarr_path = data_path / "xenium.zarr"
 logging_path = output_path / "logging"
+
+# Confirm directories exist
+if not Path(base_dir).exists():
+    raise FileNotFoundError(f"Input path {base_dir} does not exist.")
+if not Path(output_path).exists():
+    output_path.mkdir(parents=True, exist_ok=True)
+if not Path(data_path).exists():
+    data_path.mkdir(parents=True, exist_ok=True)
+if not Path(zarr_path).exists():
+    zarr_path.mkdir(parents=True, exist_ok=True)

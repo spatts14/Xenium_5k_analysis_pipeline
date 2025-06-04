5dafd7ff833adbcf82413a10b540698ec36cdc71
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2025-06-04 11:32:26 +0100

    also update package name in test

--- tests/test_helper_function.py
+++ tests/test_helper_function.py
@@ -5,7 +5,7 @@ import os
 
 def test_seed_everything():
     """Test the seed_everything function."""
-    from recode_spatial_transcriptomics.helper_function import seed_everything
+    from recode_st.helper_function import seed_everything
 
     assert os.environ.get("PYTHONHASHSEED") is None
 

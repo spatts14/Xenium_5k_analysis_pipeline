6fbad6ac7510f04f9f7b0d91b5b0104f6481876d
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2025-06-27 15:32:15 +0100

    Create __main__.py and call modules from there

---
+++ src/recode_st/__main__.py
@@ -0,0 +1,46 @@
+"""The main entry point for the recode_st package."""
+
+from logging import getLogger
+
+from recode_st.annotate import run_annotate
+from recode_st.dimension_reduction import run_dimension_reduction
+from recode_st.format import run_format
+from recode_st.logging_config import configure_logging
+from recode_st.muspan import run_muspan
+from recode_st.qc import run_qc
+from recode_st.spatial_statistics import run_spatial_statistics
+from recode_st.view_images import run_view_images
+
+logger = getLogger("recode_st")
+
+
+def main():
+    """Main function to run the recode_st package."""
+    configure_logging()
+
+    logger.info("Starting recode_st pipeline...")
+
+    logger.info("Running Module 0 - Format")
+    run_format()
+
+    logger.info("Running Module 1 - Quality Control")
+    run_qc()
+
+    logger.info("Running Module 2 - Dimension Reduction")
+    run_dimension_reduction()
+
+    logger.info("Running Module 3 - Annotate")
+    run_annotate()
+
+    logger.info("Running Module 4 - View Images")
+    run_view_images()
+
+    logger.info("Running Module 5 - Spatial Statistics")
+    run_spatial_statistics()
+
+    logger.info("Running Module 6 - MuSpAn")
+    run_muspan()
+
+
+if __name__ == "__main__":
+    main()

684672679c375f42d0838824c6028e14d0e69018
Sara Patti <sep22@ic.ac.uk>
2025-06-27 15:17:15 +0100

    building out muspan analysis, created domain object, still need to filter adata on selected area and add cell_ids to domain

--- src/recode_st/6_muspan.py
+++ src/recode_st/6_muspan.py
@@ -3,12 +3,12 @@
 import warnings
 from logging import getLogger
 
+import muspan as ms
 import scanpy as sc
-import spatialdata as sd
 
 from recode_st.helper_function import seed_everything
 from recode_st.logging_config import configure_logging
-from recode_st.paths import output_path, zarr_path
+from recode_st.paths import area_path, output_path, xenium_path
 
 warnings.filterwarnings("ignore")
 
@@ -30,6 +30,15 @@ def run_muspan():
     module_name = "6_muspan"  # name of the module
     module_dir = output_path / module_name
     seed = 21122023  # seed for reproducibility
+    domain_name = "Xenium_lung_FFPE"  # name of the domain
+    transcripts_of_interest = [
+        "EPCAM",
+        "CD3D",
+        "CD68",
+        "VWF",
+        "PTPRC",
+        "ACTA2",
+    ]
 
     # Set seed
     seed_everything(seed)
@@ -39,8 +48,38 @@ def run_muspan():
 
     # Import data
     logger.info("Loading Xenium data...")
-    adata = sc.read_h5ad(output_path / "5_spatial_stats" / "adata.h5ad")  # noqa: F841
-    sdata = sd.read_zarr(zarr_path)  # noqa: F841
+    adata = sc.read_h5ad(output_path / "5_spatial_stats" / "adata.h5ad")
+
+    # Import Xenium data using muspan
+    tofi = transcripts_of_interest
+
+    # Create muspan object
+    logger.info("Creating MuSpAn domain object...")
+    domain = ms.io.xenium_to_domain(
+        path_to_xenium_data=str(xenium_path),  # Convert Path to string
+        cells_from_selection_csv=str(area_path),  # Convert Path to string
+        domain_name=domain_name,
+        load_transcripts=True,
+        selected_transcripts=tofi,
+        load_nuclei=True,
+        load_cells_as_shapes=True,
+        exclude_no_nuclei_cells=True,
+    )
+    logger.info(f"Domain object created: {domain}")
+
+    # Filer adata only to include cells within the area of interest
+    logger.info("Filtering adata to include only cells in the area of interest...")
+    domain_cell_ids = domain.labels["Cell ID"]  # Get cell IDs from the domain
+    filt_adata = adata[
+        adata.obs_names.isin(domain_cell_ids)
+    ].copy()  # filter adata object
+    logger.info(f"Filtered adata from {adata.n_obs} to {filt_adata.n_obs} cells")
+
+    # Add cell cluster IDs
+    logger.info("All cell_type IDs to domain with cluster labels")
+    cell_type = filt_adata.obs["cell_type"]  # filter on cell type column
+    domain.add_labels(label_name="cell_type", labels=cell_type)
+    logger.info(f"Label keys in domain: {domain.labels.keys()}")
 
 
 if __name__ == "__main__":

36c7a0db9c3c69615f36340e42a7f13abb68ff48
Sara Patti <sep22@ic.ac.uk>
2025-10-15 21:23:55 +0100

    update hpc bash script

--- HPC_jobs/hpc_run.sh
+++ HPC_jobs/hpc_run.sh
@@ -9,7 +9,7 @@ module load tools/prod
 module load Biopython/1.84-foss-2024a
  
 # Change to directory
-cd "$PBS_O_WORKDIR"
+cd /rds/general/user/sep22/home/Projects/Xenium_5k_analysis_pipeline
 
 # Activate virtual environment
 source xenium_5k_venv/bin/activate

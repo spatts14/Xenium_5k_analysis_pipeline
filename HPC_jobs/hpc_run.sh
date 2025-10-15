c31d115ee7573e89d9a58a25069068b30077916d
Sara Patti <sep22@ic.ac.uk>
2025-10-15 13:51:12 +0100

    created bash file to run xenium_analysis on HPC

---
+++ HPC_jobs/hpc_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -l walltime=1:0:0
+#PBS -lselect=1:ncpus=1:mem=64gb
+
+# Load production tools
+module load tools/prod
+ 
+# Load python and bundle
+module load Biopython/1.84-foss-2024a
+ 
+# Change to directory
+cd /rds/general/user/sep22/home/Projects/Xenium_5k_analysis_pipeline
+
+# Activate virtual environment
+source xenium_5k_venv/bin/activate
+
+# Run
+python -m recode_st config_HPC.toml

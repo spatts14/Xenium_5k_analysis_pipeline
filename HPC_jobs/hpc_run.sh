f77a32df9d3cfea5d274ee45862be1e5f873c8ea
Sara Patti <sep22@ic.ac.uk>
2025-10-16 09:39:19 +0100

    updated HPC scripts

--- HPC_jobs/hpc_run.sh
+++ HPC_jobs/hpc_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #PBS -l walltime=4:0:0
-#PBS -lselect=1:ncpus=8:mem=64gb
+#PBS -lselect=1:ncpus=16:mem=128gb
 
 # Load production tools
 module load tools/prod
- 
+
 # Load python and bundle
 module load Biopython/1.84-foss-2024a
- 
+
 # Change to directory
 cd /rds/general/user/sep22/home/Projects/Xenium_5k_analysis_pipeline
 

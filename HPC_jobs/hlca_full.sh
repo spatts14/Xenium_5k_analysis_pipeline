4feccb9d38cbeb7b9dcd4f5b9a244a56495cdf7d
Sara Patti <sep22@ic.ac.uk>
2025-11-02 19:14:29 +0000

    bash script to run hlca

---
+++ HPC_jobs/hlca_full.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#PBS -l walltime=4:0:0
+#PBS -lselect=1:ncpus=16:mem=128gb
+
+# Load production tools
+module load tools/prod
+
+# Load python and bundle
+module load Biopython/1.84-foss-2024a
+
+# Change to directory
+cd /rds/general/user/sep22/home/Projects/Xenium_5k_analysis_pipeline
+
+# Activate virtual environment
+source xenium_5k_venv/bin/activate
+
+# Run
+python /Users/sarapatti/PhD_projects/Xenium_5k_analysis_pipeline/src/manual_src/hlca_full.py

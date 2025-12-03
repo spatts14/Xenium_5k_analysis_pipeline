e932f2b186ff4584045e913b9f8365faf3283d6c
Sara Patti <sep22@ic.ac.uk>
2025-12-03 20:12:04 +0000

    configure to run on GPU

--- HPC_jobs/hpc_run_int.sh
+++ HPC_jobs/hpc_run_int.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#PBS -l walltime=5:0:0
-#PBS -lselect=1:ncpus=16:mem=512gb
+#PBS -l walltime=8:0:0
+#PBS -lselect=1:ncpus=32:mem=512gb:ngpus=1:gpu_type=A100
+#PBS -q gpu
 
 # Load production tools
 module load tools/prod
@@ -8,11 +9,17 @@ module load tools/prod
 # Load python and bundle
 module load Biopython/1.84-foss-2024a
 
+# Load CUDA if needed
+module load CUDA/12.1.1
+
 # Change to directory
 cd /rds/general/user/sep22/home/Projects/Xenium_5k_analysis_pipeline
 
 # Activate virtual environment
 source xenium_5k_venv/bin/activate
+
+# Set GPU environment variables
+export CUDA_VISIBLE_DEVICES=0
 
 # Run
 python -m recode_st config_files/config_HPC_runs_int.toml

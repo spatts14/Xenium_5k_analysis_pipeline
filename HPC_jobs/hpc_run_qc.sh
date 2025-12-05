8362856e94d1f04f47ed6abeee99cc72514ba16b
Sara Patti <sep22@ic.ac.uk>
2025-12-05 00:30:41 +0000

    increased resoruce request

--- HPC_jobs/hpc_run_qc.sh
+++ HPC_jobs/hpc_run_qc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l walltime=1:0:0
-#PBS -lselect=1:ncpus=8:mem=256gb
+#PBS -l walltime=10:0:0
+#PBS -lselect=1:ncpus=16:mem=512gb
 
 # Load production tools
 module load tools/prod

2f86fe1345c59d4297fc89e830761778a5217fe5
Sara Patti <sep22@ic.ac.uk>
2026-02-17 13:50:40 +0000

    rerun QC

--- HPC_jobs/airscape_hpc.sh
+++ HPC_jobs/airscape_hpc.sh
@@ -15,11 +15,11 @@ cd /rds/general/user/sep22/home/Projects/Xenium_5k_analysis_pipeline
 source xenium_5k_venv/bin/activate
 
 # Run
-# echo "Starting at annotation... $(date)"
-# python -m recode_st config_files/airscape_annotate.toml
+echo "Starting at annotation... $(date)"
+python -m recode_st config_files/airscape_annotate.toml
 
 # echo "Creating adata for each cell type... $(date)"
 # python src/manual_src/celltype_subset.py
 
-echo "Subset each cell type... $(date)"
-python src/manual_src/celltype_level_1.py
+# echo "Subset each cell type... $(date)"
+# python src/manual_src/celltype_level_1.py

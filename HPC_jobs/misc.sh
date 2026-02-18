0d832c658ffa1cedbd30ad13d1e8208773a9c739
Sara Patti <sep22@ic.ac.uk>
2026-02-18 20:03:57 +0000

    add a .sh file to run misc analysis on HPC

---
+++ HPC_jobs/misc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -l walltime=12:0:0
+#PBS -lselect=1:ncpus=1:mem=256gb
+#PBS -N subset_celltype
+#PBS -j oe
+
+# Load production tools
+module load tools/prod
+
+# Load python and bundle
+module load Biopython/1.84-foss-2024a
+
+# Change to directory
+cd /rds/general/user/sep22/home/Projects/Xenium_5k_analysis_pipeline
+
+# Activate virtual environment
+source xenium_5k_venv/bin/activate
+
+# Run with error logging
+echo "Starting at $(date)"
+
+python src/manual_src/misc_plots.py
+
+echo "Completed at $(date)"

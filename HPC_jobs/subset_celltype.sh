578d0e961e8b1d71bf4a6c7e98859737fe6cdbb3
Sara Patti <sep22@ic.ac.uk>
2026-02-20 19:33:55 +0000

    run all major cell types

--- HPC_jobs/subset_celltype.sh
+++ HPC_jobs/subset_celltype.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l walltime=12:0:0
-#PBS -lselect=1:ncpus=1:mem=256gb
+#PBS -lselect=1:ncpus=1:mem=128gb
 #PBS -N subset_celltype
 #PBS -j oe
 
@@ -21,6 +21,38 @@ echo "Starting at $(date)"
 
 # make new adata for each major cell type
 #python src/manual_src/celltype_subset.py
+
+# Defaults can be overridden when submitting the job
+: "${H5AD_FILE:=adata_subset_Stromal_cells.h5ad}"
+: "${SUBSET:=stromal}"
+
+# high resolution clustering and plotting for each major cell type
+python src/manual_src/celltype_level_1.py
+
+# Defaults can be overridden when submitting the job
+: "${H5AD_FILE:=adata_subset_Immune_cells.h5ad}"
+: "${SUBSET:=immune}"
+
+# high resolution clustering and plotting for each major cell type
+python src/manual_src/celltype_level_1.py
+
+# Defaults can be overridden when submitting the job
+: "${H5AD_FILE:=adata_subset_Endothelial_cells.h5ad}"
+: "${SUBSET:=endothelial}"
+
+# high resolution clustering and plotting for each major cell type
+python src/manual_src/celltype_level_1.py
+
+# Defaults can be overridden when submitting the job
+: "${H5AD_FILE:=adata_subset_Alveolar_epithelial_cells.h5ad}"
+: "${SUBSET:=alveolar_epithelial}"
+
+# high resolution clustering and plotting for each major cell type
+python src/manual_src/celltype_level_1.py
+
+# Defaults can be overridden when submitting the job
+: "${H5AD_FILE:=adata_subset_Airway_epithelial_cells.h5ad}"
+: "${SUBSET:=airway_epithelial}"
 
 # high resolution clustering and plotting for each major cell type
 python src/manual_src/celltype_level_1.py

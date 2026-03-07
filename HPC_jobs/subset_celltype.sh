527f501c4f2677972381a5ae542455877b003eaf
Sara Patti <sep22@ic.ac.uk>
2026-03-07 19:06:28 +0000

    update path

--- HPC_jobs/subset_celltype.sh
+++ HPC_jobs/subset_celltype.sh
@@ -18,7 +18,7 @@ source xenium_5k_venv/bin/activate
 
 # Set directory paths
 export ANNOTATE_DIR="/rds/general/user/sep22/projects/phenotypingsputumasthmaticsaurorawellcomea1/live/Sara_Patti/009_ST_Xenium/output/2026-02-22_analysis_run_HVG2000"
-export CELLTYPE_SUBSET_DIR="/rds/general/user/sep22/projects/phenotypingsputumasthmaticsaurorawellcomea1/ephemeral/celltype_subset_HVG2000"
+export CELLTYPE_SUBSET_DIR="/rds/general/user/sep22/projects/phenotypingsputumasthmaticsaurorawellcomea1/live/Sara_Patti/009_ST_Xenium/output/2026-02-22_analysis_run_HVG2000/celltype_subset"
 
 # Run with error logging
 echo "Starting at $(date)"

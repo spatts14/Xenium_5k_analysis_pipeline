4d86f8e09e399f04783783461c58d6d1cbaee237
Sara Patti <sep22@ic.ac.uk>
2026-03-12 21:48:20 +0000

    rerun airway epi cells

--- src/manual_src/misc_analysis/celltype_level_1.py
+++ src/manual_src/misc_analysis/celltype_level_1.py
@@ -493,18 +493,23 @@ level_0 = "level_0_annotation"
 res_list = [0.5]
 
 # Resolution to use for mapping clusters to annotations
-resolution = 0.3
-chosen_resolution_name = f"Alveolar_epithelial_{resolution}"
+resolution = 0.5
+# chosen_resolution_name = f"Immune_cells_{resolution}"
+chosen_resolution_name = f"Airway_epithelial_cells_{resolution}"
 # Annotate clusters based on marker genes and plot UMAP
 annotation_dict = {
-    "0": "Intermediate AT cells and Unknown",
-    "1": "AT2 cells 1",
-    "2": "AT1 cells",
-    "3": "AT2 cells 2",
-    "4": "AT2 cells 3",
-    "5": "Proliferating AT2 cells",
-    "6": "AT2 cells 4",
-    "7": "AT2 cells 5",
+    "0": "Ciliated cells 1",
+    "1": "Ciliated cells 2",
+    "2": "Goblet cells 1",
+    "3": "Goblet cells 2",
+    "4": "Goblet cells 3",
+    "5": "Unknown - Stromal",
+    "6": "Ciliated cells 3",
+    "7": "Basal cells 1",
+    "8": "Basal cells 2",
+    "9": "Basal cells 3",
+    "10": "Secretory epithelial cells",
+    "11": "Proliferating Basal cells",
 }
 
 

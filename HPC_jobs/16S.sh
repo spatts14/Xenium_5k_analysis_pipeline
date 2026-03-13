754c14e35efee93f56f8661c70fb9595e0b71c3a
Sara Patti <sep22@ic.ac.uk>
2026-03-13 13:34:11 +0000

    .sh file to run 16S plots

---
+++ HPC_jobs/16S.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+#PBS -l walltime=24:0:0
+#PBS -lselect=1:ncpus=1:mem=256gb
+#PBS -N subset_celltype
+#PBS -j oe
+
+# Load production tools
+module load tools/prod
+
+# Load python and bundle
+module load Biopython/1.84-foss-2024a
+
+# Change to directory
+cd /rds/general/user/sep22/home/Projects/Xenium_5k_analysis_pipeline
+
+# Activate virtual environment
+source xenium_5k_venv/bin/activate
+
+# Run script
+python /src/manual_src/misc_analysis/16S/plots.py

0b6621f75299b140169ab765cb19647ae13a32f2
Sara Patti <sep22@ic.ac.uk>
2026-03-13 11:59:59 +0000

    removed space that was causing a bug

--- HPC_jobs/subset_adata.sh
+++ HPC_jobs/subset_adata.sh
@@ -18,7 +18,7 @@ cd /rds/general/user/sep22/home/Projects/Xenium_5k_analysis_pipeline
 source xenium_5k_venv/bin/activate
 
 # Set directory paths
-export BASE_DIR ="/rds/general/user/sep22/projects/phenotypingsputumasthmaticsaurorawellcomea1/live/Sara_Patti/009_ST_Xenium/output/2026-02-22_analysis_run_HVG2000/"
+export BASE_DIR="/rds/general/user/sep22/projects/phenotypingsputumasthmaticsaurorawellcomea1/live/Sara_Patti/009_ST_Xenium/output/2026-02-22_analysis_run_HVG2000/"
 
 # Run with error logging
 echo "Starting at $(date)"

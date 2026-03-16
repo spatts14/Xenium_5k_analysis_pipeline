57b9e4669e20326295c990c9358531bfcf320669
Sara Patti <sep22@ic.ac.uk>
2026-03-16 09:53:36 +0000

    16S expression on IPF and COPD only

--- src/manual_src/misc_analysis/16S/plots.py
+++ src/manual_src/misc_analysis/16S/plots.py
@@ -23,63 +23,59 @@ sc.settings.figdir = fig_dir
 # Load data
 adata = sc.read_h5ad(dir / "subset_adata/adata_subset_COPD_IPF_PM08.h5ad")
 
+# Subset on IPF and PM08
+adata = adata[adata.obs["condition"].isin(["IPF", "PM08"])].copy()
+
 # Remove unknown cell types
-adata = adata[adata.obs["level_0_annotation"] != "Unknown"].copy()
+adata = adata[~adata.obs["level_0_annotation"].str.contains("Unknown", case=False)]
 print(adata.obs["level_0_annotation"].value_counts())
 
-# # Plots
-# sc.pl.umap(
-#     adata,
-#     color=["condition", "timepoint", "level_0_annotation"],
-#     save="_conditions.png",
-# )
+# Look at 16S expression by cell type
+sc.pl.dotplot(
+    adata,
+    ["16S", "KRT5", "MRC1"],
+    groupby="level_0_annotation",
+    cmap=cmap,
+    save="_level_0.pdf",
+)
 
-# # Look at 16S expression by cell type
-# sc.pl.dotplot(
-#     adata,
-#     ["16S", "KRT5", "MRC1"],
-#     groupby="level_0_annotation",
-#     cmap=cmap,
-#     save="_level_0.pdf",
-# )
+# Look at 16S expression by cell type
+sc.pl.dotplot(
+    adata, ["16S"], groupby="level_0_annotation", cmap=cmap, save="_level_0_16S.pdf"
+)
 
-# # Look at 16S expression by cell type
-# sc.pl.dotplot(
-#     adata, ["16S"], groupby="level_0_annotation", cmap=cmap, save="_level_0_16S.pdf"
-# )
+# Look at 16S expression by cell type
+sc.pl.dotplot(
+    adata,
+    ["16S", "KRT5", "MRC1"],
+    groupby="condition",
+    cmap=cmap,
+    save="_condition.png",
+)
 
-# # Look at 16S expression by cell type
-# sc.pl.dotplot(
-#     adata,
-#     ["16S", "KRT5", "MRC1"],
-#     groupby="condition",
-#     cmap=cmap,
-#     save="_condition.png",
-# )
+# Look at 16S expression by cell type
+sc.pl.dotplot(adata, ["16S"], groupby="condition", cmap=cmap, save="_condition_16S.pdf")
 
-# # Look at 16S expression by cell type
-# sc.pl.dotplot(adata, ["16S"], groupby="condition", cmap=cmap, save="_condition_16S.pdf")
+sc.pl.violin(adata, ["16S"], groupby="condition", save="_violin_16S.png")
 
-# sc.pl.violin(adata, ["16S"], groupby="condition", save="_violin_16S.png")
-
-# sc.pl.matrixplot(adata, ["16S"], groupby="condition", cmap=cmap, save="_matrix_16S.pdf")
+sc.pl.matrixplot(adata, ["16S"], groupby="condition", cmap=cmap, save="_matrix_16S.pdf")
 
 
-# # Plot per cell type
-# cell_type_list = adata.obs["level_0_annotation"].unique()
+# Plot per cell type
+cell_type_list = adata.obs["level_0_annotation"].unique()
 
-# for cell in cell_type_list:
-#     resolution = "level_0_annotation"
-#     adata_subset = adata[adata.obs[resolution] == cell].copy()
+for cell in cell_type_list:
+    resolution = "level_0_annotation"
+    adata_subset = adata[adata.obs[resolution] == cell].copy()
 
-#     sc.pl.dotplot(
-#         adata_subset,
-#         ["16S"],
-#         groupby="condition",
-#         title=f"{cell}",
-#         cmap=cmap,
-#         save=f"_{cell}_condition.pdf",
-#     )
+    sc.pl.dotplot(
+        adata_subset,
+        ["16S"],
+        groupby="condition",
+        title=f"{cell}",
+        cmap=cmap,
+        save=f"_{cell}_condition.pdf",
+    )
 
 # Plot spatial plots
 color_list = ["level_0_annotation", "level_1_annotation", "16S"]
